72a2724ad9bd8f41233d4db0ee61f9dc9f2fbda7
Jpdelas <jpmadeinzimba@gmai.com>
2023-10-08 17:52:36 -0300

    mudança na paleta de cores e mais ajustes

--- scripts/get_citie.js
+++ scripts/get_citie.js
@@ -143,6 +143,8 @@ async function request(citie_name) {
 function transition(){
     document.querySelector('.catch-citie').style.display = "none";
     document.querySelector('.information').style.display = "flex";
+    
+    
 
     /*citie_info*/
     document.querySelector('.citie_info .nome').innerHTML = citie_info.name;
@@ -175,7 +177,6 @@ function transition(){
     document.querySelector('.country_info .populacao').innerHTML = "População: " + country_info.population + " de pessoas";
     document.querySelector('.country_info .bandeira').src = country_info.flag_link;
 }
-
 function create_map(){
     document.querySelector('.citie_map').innerHTML = `<iframe class="iframe_map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1760.9649263023773!2d${citie_info.lon}!3d${citie_info.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1696557851166!5m2!1spt-BR!2sbr"  width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>`;
 }
