3d30440f946b7234e874a836415814593f102bd1
Jpdelas <jpmadeinzimba@gmai.com>
2023-10-07 23:36:07 -0300

    Mudança no tamanho do mapa

--- scripts/get_citie.js
+++ scripts/get_citie.js
@@ -177,7 +177,7 @@ function transition(){
 }
 
 function create_map(){
-    document.querySelector('.citie_map').innerHTML = `<iframe class="iframe_map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1760.9649263023773!2d${citie_info.lon}!3d${citie_info.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1696557851166!5m2!1spt-BR!2sbr"></iframe>`;
+    document.querySelector('.citie_map').innerHTML = `<iframe class="iframe_map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1760.9649263023773!2d${citie_info.lon}!3d${citie_info.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1696557851166!5m2!1spt-BR!2sbr"  width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>`;
 }
 
 form.addEventListener("submit", (event) => {
