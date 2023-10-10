c4d9f973c8148819e5d4ad2c2e947063629479f4
Perera <Perera@gmail>
2023-10-09 23:57:09 -0300

    modal script

--- scripts/style_script.js
+++ scripts/style_script.js
@@ -2,6 +2,8 @@ const btnBack = document.querySelector('.btn-back');
 const information = document.querySelector('.information');
 const catchCitie = document.querySelector('.catch-citie');
 const logo = document.querySelector('#logo');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.modal span');
 
 btnBack.addEventListener('click', () => {
     information.style.display = 'none';
@@ -9,4 +11,13 @@ btnBack.addEventListener('click', () => {
     logo.style.display = 'block';
     document.querySelector('#citie_input').value = "";
     document.querySelector('#citie_input').focus();
+});
+
+if(sessionStorage.log == null){
+    modal.style.display = 'flex';
+    sessionStorage.log = true;
+}
+
+close.addEventListener('click', () => {
+    modal.style.display = 'none';
 });
