88e452d39ded6a3961c8b4783a2afff854b04ad7
Jpdelas <jpmadeinzimba@gmai.com>
2023-10-09 22:25:37 -0300

    mudanças logo

--- scripts/style_script.js
+++ scripts/style_script.js
@@ -1,8 +1,12 @@
 const btnBack = document.querySelector('.btn-back');
 const information = document.querySelector('.information');
 const catchCitie = document.querySelector('.catch-citie');
+const logo = document.querySelector('#logo');
 
 btnBack.addEventListener('click', () => {
     information.style.display = 'none';
     catchCitie.style.display = 'block';
+    logo.style.display = 'block';
+
+
 });
