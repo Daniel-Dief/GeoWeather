4ac4e29806f25ee6d44689002263228cd1248a6e
Jpdelas <jpmadeinzimba@gmai.com>
2023-10-10 21:05:55 -0300

    responsividade parcial, ajustes no script do modal

--- scripts/style_script.js
+++ scripts/style_script.js
@@ -4,6 +4,7 @@ const information = document.querySelector('.information');
 const catchCitie = document.querySelector('.catch-citie');
 const logo = document.querySelector('#logo');
 const modal = document.querySelector('.modal');
+const modalButton = document.querySelector('.modal-button');
 const close = document.querySelector('.modal span');
 
 btnBack.addEventListener('click', () => {
@@ -18,12 +19,21 @@ if (sessionStorage.log == null) {
     modal.style.display = 'flex';
     document.querySelector('.dark-overlay').style.zIndex = 0;
     sessionStorage.log = true;
-    logo.style.display = 'none'; 
+    logo.style.display = 'none';
 }
 
-body.addEventListener('click', () => {
+document.addEventListener('click', (e) => {
+    if (modal.style.display === 'flex' && !modal.contains(e.target)) {
+        modal.style.display = 'none';
+        document.querySelector('.dark-overlay').style.zIndex = -1;
+        document.querySelector('.catch-citie input').focus();
+        logo.style.display = 'block';
+    }
+});
+
+modalButton.addEventListener('click', () => {
     modal.style.display = 'none';
     document.querySelector('.dark-overlay').style.zIndex = -1;
     document.querySelector('.catch-citie input').focus();
-    logo.style.display = 'block'; 
+    logo.style.display = 'block';
 });
